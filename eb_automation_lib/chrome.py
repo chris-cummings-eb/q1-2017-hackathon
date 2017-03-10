252e17dd74808beee871a2799400b6467ec89cfe
chris-cummings-eb <ccummings@eventbrite.com>
2017-03-09 21:22:36 -0600

    refactor chrome, fix some bugs, clean up window_hlpers

--- eb_automation_lib/chrome.py
+++ eb_automation_lib/chrome.py
@@ -9,6 +9,8 @@ from .task import Task, Queue
 # utilites and helpers from the automation library
 from .utils.helper_functions import *
 from .utils.window_helpers import (
+    get_window_size,
+    create_window_position,
     set_window_size_by_percent
 )
 
@@ -48,22 +50,10 @@ class Chrome(object):
     # The methods here at the top are meant for direct use in the automation hackathon course and are
     # intended to serve as an example for how to build your own automations and provide some useful,
     # high-level building-blocks suited for common Eventbrite workflows.
-    #
-    # PROTIP: Take advantage of the open_event_[LOCATION]_page methods in the next section
     # ---------------------------------------------------
-    def open_salesforce_eventbrite_admin_side_by_side(self, salesforce_query, eventbrite_query):
-        """A sample method for how to view two customized things at once"""
-
-        sf_win, sf_tab = self.new_window_with_tab(url="https://salesforce.com")
-        eb_win, eb_tab = self.new_window_with_tab(url=admin_search_url(eventbrite_query))
-        self.vertical_split_window(window=sf_win, side="left")
-        self.vertical_split_window(window=eb_win, side="right")
-
-        return sf_tab, eb_tab
-
     def get_eventbrite_tab(self):
         """returns the first eventbrite tab object"""
-        eventbrite_tabs = self.tabs_with_urls_that_contain_pattern("eventbrite")[0]
+        eventbrite_tabs = self.tabs_with_urls_that_contain_pattern("eventbrite")
         if eventbrite_tabs:
             return eventbrite_tabs[0]
 
@@ -73,106 +63,73 @@ class Chrome(object):
         if salesforce_tabs:
             return salesforce_tabs[0]
 
-    def order_lookup(self, orders, environment="production"):
-        """
-        opens a new tab in Evenbrite Admin Order Lookup, searching the list of orders
-        returns the tab object or the created tab
-        """
-        if not isinstance(orders, list):
-            orders = [orders]
-
-        url = eventbrite_url_constructor(
-            "admin/orderinfo/",
-            parameters={"order_id": ",".join(orders)}
-        )
-        for order in orders:
-            if not is_order_id(order):
-                raise ValueError("Order list contains invalid order ids. {} is not an order id".format(order))
-
-        return self.new_tab_at_url(url)
-
-    def search_eventbrite_admin(self, query, environment="production", new_tab=True):
-        """opens a new tab in Eventbrite Admin search, returns the tab object for the created tab"""
-        url = admin_search_url(query)
-        if new_tab:
-            return self.new_tab_at_url(url)
-        else:
-            admin_tabs = self.tabs_with_urls_that_contain_pattern("admin.eventbrite")
-            if admin_tabs:
-                admin_tab = admin_tabs[0]
-                admin_tab._set_tab_url_to(url)
-                return admin_tab
-            else:
-                return self.new_tab_at_url(url)
-
-    # ---------------------------------------------------
-    # open_event_[LOCATION] methods simply take an event_id and open a new tab at that location.
-    # These methods were made for you to quickly maneuver about the eventbrite platform in your automations
-    # all of the methods in this section return the tab object that they create
-    # ---------------------------------------------------
-
-    def open_event_listing(self, event_id, environment="production", parameters=None):
-        """opens a new tab at the event's listing page, returns the tab object for the created tab"""
-        return self._open_event_page_location(event_id, "e")
-
-    def open_event_edit_page(self, event_id, environment="production"):
-        """opens a new tab on the Edit Page of an event id, returns the tab object"""
-        return self._open_event_page_location(event_id, "edit")
-
-    def open_event_manage_page(self, event_id, environment="production"):
-        """opens a new tab on the Manage Page of an event id, returns the tab object"""
-        return self._open_event_page_location(event_id, "myevent")
-
-    # --------
-    # TODO: add all of the location methods
-    # --------
-
     # ---------------------------------------------------
     # The methods below are building block methods. They're useful for building more customized automations
     # from scratch, or that aren't specific to the primary Eventbrite workflows
     # ---------------------------------------------------
 
     def active_tab(self, window_index=0):
-        """
-        returns the tab that currently has focus i.e. the front-most tab
-        """
+        """returns the tab that currently has focus i.e. the front-most tab"""
         return self.windows()[window_index].activeTab()
 
-    def new_tab_at_url(self, url, window_index=0):
-        """opens a new tab in a window at the specified url, returns the new tab object"""
-        tab = self._create_tab_object({
-            "URL": url
-        })
-        self._append_tab_to_window(tab, window_index)
+    def new_tab(self, url=None, window_index=0):
+        """create a new tab, returns the tab"""
+        props = {}
+        if url:
+            props['URL'] = url
 
+        tab = self._create_tab_object(self, properties=props)
+        self._append_tab_to_window(tab, window_index=window_index)
         return tab
 
-    def new_window_with_tab(self, url=None):
-        """creates a new window with a tab open at the specified url, returns the window and the tab"""
-        window = self._create_window_object()
-
-        if url is not None:
-            # create new tab at url
-            tab = self._create_tab_object({"URL": url})
-
+    def new_window(self, url=None):
+        """create a new window. If url is specified, the tab is set to that location
+        returns the window and tab
+        """
+        window = self._create_window_object(properties=properties)
         self._append_window_to_application(window)
-        self._append_tab_to_window(tab)
+        if url:
+            tab = self.new_tab({"URL": url}, window_index=window.index())
+            for t in window.tabs():
+                if t.id() != tab.id():
+                    t.close()
+            return window, tab
 
-        # close default tab if new tab was created
-        tabs = window.tabs()
-        if len(tabs) > 1:
-            tabs[0].close()
+        return window, window.tabs()[0]
 
-        return window, tab
+    def open_event_page_location(self, event_id, path):
+        """convenience method for opening various event pages (ie manage, waitlist etc)"""
+        if is_event_id(event_id):
+            url = eventbrite_url_constructor(path, parameters={"eid": event_id})
+            return self.new_tab(url)
+        else:
+            raise ValueError("{} is not a valid Eventbrite Event ID".format(event_id))
 
-    def vertical_split_window(self, side="left", window=0):
-        """resizes the window passed in or at the window_index and returns the size data"""
-        if isinstance(window, int):
-            window = self.windows(window)
-        return set_window_size_by_percent(window, 50, 100, position=("upper", side))
+    def set_tab_url_to(self, tab, url):
+        """uses js to send a tab to a new url"""
+        self._execute_javascript_in_tab(tab, """window.location = {}""".format(url))
 
-    def tabs(self):
-        """returns a list of tab objects for each tab in each window"""
+    @staticmethod
+    def set_window_position_to(window, position=('upper', 'right')):
+        """set the position a window to ('upper', 'left'), ('lower', 'left'),
+        ('upper', 'right'), or ('lower', 'right')
+        """
+        window.setPosition_(
+            create_window_position(get_window_size(window), position)
+        )
+
+    def tabs(self, tab_index=None, window_index=None):
+        """returns a list of tab objects for each tab in each window
+        if tab_index is given, returns the tab at the given index of the 0th window, or the given
+        window_index
+        """
+        if window_index and tab_index:
+            return self.windows(window_index).tabs()[tab_index]
+        elif window_index:
+            return self.windows(window_index).tabs()
+        elif tab_index:
+            return self.windows(0).tabs()[0]
+
         return [tab for window in self.windows() for tab in window.tabs()]
 
     def tabs_at_url(self, url):
@@ -195,6 +152,10 @@ class Chrome(object):
         """
         return [t for t in self.tabs() if re.search(pattern, t.URL())]
 
+    def vertical_split_window(self, window_index, side="left"):
+        """resizes the window at window index and positions it to the left or right side"""
+        return set_window_size_by_percent(self.window(window_index), 50, 100, position=("upper", side))
+
     def windows(self, index=None):
         """returns a list of all open window objects or the window at the specified index"""
         windows = self.driver.windows()
@@ -210,63 +171,48 @@ class Chrome(object):
         self.windows()[window_index].tabs().append(tab_object)
 
     def _append_window_to_application(self, window_object):
-        """effectively, this method is just "drawing" the window"""
+        """add an initialized window to chrome"""
         self.windows().append(window_object)
 
-    @staticmethod
-    def _click_DOM_element(tab, element_id):
+    def _block_until_loaded(self, tab):
+        while tab.loading():
+            pass
+
+    def _click_DOM_element(self, tab, element_id):
         """use javascript to click an element in a tab's DOM"""
         js = get_javascript_from_file("./javascript/snippets.js", export="clickElement")
-        tab.executeJavasctipt_(js)
+        self._execute_javascript_in_tab(tab, js)
 
     def _create_tab_object(self, properties={}):
         """
-        helper method to allocate and initialze tab objects
-        call this with a properties dict to initialize the object with props
-        URL is actually the only writeable property...
+        returns an allocated and initialized tab object
+        call this with a properties dict to initialize the tab at a url
         >>>self._create_tab_object({ "URL": "https://www.eventbrite.com/" })
         <GoogleChromeTab object>
         """
         return self.driver.classForScriptingClass_("tab").alloc().initWithProperties_(properties)
 
     def _create_window_object(self, properties={}):
+        """returns an allocated and initialized pyobjc window object"""
         return self.driver.classForScriptingClass_("window").alloc().initWithProperties_(properties)
 
-    def _open_event_page_location(self, event_id, path):
-        """convenience helper method for opening various event pages (ie manage, waitlist etc)"""
-        if is_event_id(event_id):
-
-            url = eventbrite_url_constructor(path, parameters={"eid": event_id})
-            return self.new_tab_at_url(url)
-
-        else:
-            raise ValueError("{} is not a valid Eventbrite Event ID".format(event_id))
-
-    @staticmethod
-    def _do_after_tab_loads(tab, callback, *args, **kwargs):
+    def _do_after_tab_loads(self, tab, callback, *callback_args):
+        """asynchronously waits for the tab to load, then calls the callback
+        NOTE: not safe for executing javascript in multiple tabs at once
         """
-        checks the loading status of the tab every 1/10th of a second, calls the callback function
-        and returns that value after the tab has finished loading
+        def do():
+            self._block_until_loaded(tab)
+            callback(*callback_args)
+        self.queue.put(Task(do))
+
+    def _execute_javascript_in_tab(self, tab, javascript):
+        """asynchronously executes javascript in the given tab
+        NOTE: this method is not safe to use inside iterators
         """
-        while tab.loading():
-            sleep(.1)
-
-        return callback(*args, **kwargs)
-
-    @staticmethod
-    def _execute_javascript_in_tab(tab, javascript):
-        """convenience method for calling the executeJavasctipt_ method on tab objects"""
-        return tab.executeJavascript_(javascript)
-
-    @staticmethod
-    def _set_tab_url_to(tab, url):
-        """convenience method for using js to move a tab to a new url"""
-        tab.executeJavascript_("""window.location = {}""".format(url))
-
-    @staticmethod
-    def _set_window_position_to(window, position=[0, 0]):
-        """set the position of a window object in pixels"""
-        window.setPosition_(position)
+        def do():
+            self._block_until_loaded(tab)
+            tab.executeJavasctipt_(javascript)
+        self.queue.put(Task(do))
 
     def __str__(self):
         return "GoogleChrome App Scripting Bridge - {win} open windows, {tab} total tabs".format(
@@ -276,4 +222,4 @@ class Chrome(object):
 
     def __init__(self):
         self.driver = SBApplication.applicationWithBundleIdentifier_("com.google.Chrome")
-        self.queue = Queue(max_workers=8)
+        self.queue = Queue(max_workers=4)
--- eb_automation_lib/default_automations.py
+++ eb_automation_lib/default_automations.py
@@ -1,25 +1,63 @@
 from .chrome import Chrome
+chrome = Chrome()
 
 
-def open_event_listing(event_id='27849228793', environment="qa"):
-    """description text"""
-    chrome = Chrome()
-    chrome.open_event_listing(event_id, environment=environment)
+def open_event_listing(event_id, environment="production", parameters=None):
+    """opens a new tab at the event's listing page, returns the tab object for the created tab"""
+    return chrome._open_event_page_location(event_id, "e")
 
 
-def somefunc():
-    """the actual docstring
-    ##NAME: cool automation 2000
-    ##DESCRIPTION: this automation is mega cool
-    ##TYPE: chrome
+def open_event_edit_page(event_id, environment="production"):
+    """opens a new tab on the Edit Page of an event id, returns the tab object"""
+    return chrome.open_event_page_location(event_id, "edit")
+
+
+def open_event_manage_page(event_id, environment="production"):
+    """opens a new tab on the Manage Page of an event id, returns the tab object"""
+    return chrome.open_event_page_location(event_id, "myevent")
+
+
+def open_salesforce_eventbrite_admin_side_by_side(query):
+    """opens two windows side by side searching sf and admin for the same thing
+    great for viewing a customer account in both places at once
     """
-    print('somefunc')
+
+    sf_win, sf_tab = self.new_window(url="https://salesforce.com")
+    eb_win, eb_tab = self.new_window(url=admin_search_url(eventbrite_query))
+    self.vertical_split_window(sf_win.index(), side="left")
+    self.vertical_split_window(eb_win.index(), side="right")
+
+    return sf_tab, eb_tab
 
 
-def blah_blah():
-    """prints blah_blah"""
-    print('blah_blah')
+def order_lookup(orders, environment="production"):
+    """opens a new tab in Evenbrite Admin Order Lookup, searching the list of orders
+    returns the tab object or the created tab
+    """
+    if not hasattr(orders, '__iter__'):
+        orders = [].append(orders)
+
+    url = eventbrite_url_constructor(
+        "admin/orderinfo/",
+        parameters={"order_id": ",".join(orders)}
+    )
+    for order in orders:
+        if not is_order_id(order):
+            raise ValueError("Order list contains invalid order ids. {} is not an order id".format(order))
+
+    return chrome.new_tab(url)
 
 
-def ooooooooooo():
-    print('oooooooo')
+def search_eventbrite_admin(query, environment="production", new_tab=True):
+    """opens a new tab in Eventbrite Admin search, returns the tab object for the created tab"""
+    url = admin_search_url(query)
+    if new_tab:
+        return chrome.new_tab(url)
+    else:
+        admin_tabs = chrome.tabs_with_urls_that_contain_pattern("admin.eventbrite")
+        if admin_tabs:
+            admin_tab = admin_tabs[0]
+            admin_tab.set_tab_url_to(url)
+            return admin_tab
+        else:
+            return chrome.new_tab(url)
